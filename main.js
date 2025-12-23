e4a477d8b67159401f36d5f1121b2936b9465eda
ecparaibano <a92044288@gmail.com>
2025-12-23 02:07:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-function assinar(plano) {
-  alert("Você escolheu o plano: " + plano + 
-        "\n\nEm breve integração com pagamento!");
+let socios = localStorage.getItem("socios");
+socios = socios ? parseInt(socios) : 661;
+
+document.getElementById("totalSocios").innerText = socios;
+
+function mostrarComprar(plano) {
+  document.getElementById("planoEscolhido").innerText =
+    "Plano selecionado: Carcará " + plano;
+  document.getElementById("comprarBox").style.display = "block";
+}
+
+function comprar() {
+  socios++;
+  localStorage.setItem("socios", socios);
+  document.getElementById("totalSocios").innerText = socios;
+  alert("Compra simulada com sucesso!");
 }
